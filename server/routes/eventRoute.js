d29e79f9971de5c51cb45b63710bc1d0712b5dea
HarshDeo10 <harshdeogadkar@gmail.com>
2024-03-30 16:48:18 +0530

    first commit

---
+++ server/routes/eventRoute.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import eventCon from '../controllers/eventCon.js';
+
+const router = Router();
+
+// Define your routes here
+router.get('/', eventCon.getAllEvents);
+
+router.get('/:id', eventCon.getEventById);
+
+router.post('/', eventCon.createEvent);
+
+router.put('/:id', eventCon.updateEvent);
+
+router.delete('/:id', eventCon.deleteEvent);
+
+export default router;
